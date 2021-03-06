eaba379610f626b5f93a17baf3c7cf3418b3822b
HemankBajaj <hemankbajaj05@gmail.com>
2021-03-07 02:06:16 +0530

    added battle method

--- bot.py
+++ bot.py
@@ -30,22 +30,27 @@ class Player:
     def buy_tanks(self, x):
         self.coins -= 400 * x
         self.tanks += x
+        self.xp += int(2*x)
 
     def buy_fighterJets(self, x):
         self.coins -= 800 * x
         self.fighterJets += x
+        self.xp += int(5*x)
 
     def buy_soldiers(self, x):
         self.coins -= 3 * x
         self.soldiers += x
+        self.xp += int(1*x/500)
 
     def buy_bombs(self, x):
         self.coins -= 10 * x
         self.bombs += x
+        self.xp += int(x/15)
 
     def buy_meds(self, x):
         self.coins -= 25 * x
         self.meds += x
+        self.xp += int(3*x/100)
 
     def spinAndWin(self):
         self.coins -= 600
@@ -64,6 +69,109 @@ class Player:
             self.bombs += random.randint(3, 10) + random.randint(3, 10) + random.randint(4, 10)
         if prize == 'meds':
             self.meds += random.randint(5, 30) + random.randint(5, 30)
+        def battle(self, player):
+        if isinstance(player, Player):
+
+            chance_factor = random.randint(0,1)
+
+            xp_self_num = self.xp
+            xp_player_num = player.xp
+            xp_den = self.xp + player.xp
+            self_xp = 0.3*xp_self_num/xp_den
+            pl_xp = 0.3*xp_player_num/xp_den
+
+            bombs_self_num = self.bombs
+            bombs_player_num = player.bombs
+            bombs_den = self.bombs + player.bombs
+            self_bombs = 0.08 * bombs_self_num / bombs_den
+            pl_bombs = 0.08 * bombs_player_num / bombs_den
+
+            soldiers_self_num = self.soldiers
+            soldiers_player_num = player.soldiers
+            soldiers_den = self.soldiers + player.soldiers
+            self_soldiers = 0.02 * soldiers_self_num / soldiers_den
+            pl_soldiers = 0.02 * soldiers_player_num / soldiers_den
+
+            meds_self_num = self.meds
+            meds_player_num = player.meds
+            meds_den = self.meds + player.meds
+            self_meds = 0.05 * meds_self_num / meds_den
+            pl_meds = 0.05 * meds_player_num / meds_den
+
+            fighterJets_self_num = self.fighterJets
+            fighterJets_player_num = player.fighterJets
+            fighterJets_den = self.fighterJets + player.fighterJets
+            self_fighterJets = 0.20 * fighterJets_self_num / fighterJets_den
+            pl_fighterJets = 0.20 * fighterJets_player_num / fighterJets_den
+
+            tanks_self_num = self.tanks
+            tanks_player_num = player.tanks
+            tanks_den = self.tanks + player.xp
+            self_tanks = 0.15 * tanks_self_num / tanks_den
+            pl_tanks = 0.15 * tanks_player_num / tanks_den
+
+            if chance_factor == 1:
+                y = 0
+            else:
+                y = 1
+
+            self_score = self_bombs + self_xp + self_meds + self_fighterJets + self_tanks + self_soldiers + 0.2*(1-y)
+            pl_score = pl_bombs + pl_xp + pl_meds + pl_fighterJets + pl_tanks + pl_soldiers + 0.2*y
+
+            if self_score > pl_score:
+                self.victories += 1
+                player.defeats += 1
+
+                self.xp += random.randint(5,10)
+                player.xp += random.randint(1,5)
+
+                self.tanks = int(self.tanks*random.randint(60,90)/100)
+                player.tanks = int(player.tanks * random.randint(30, 60) / 100)
+
+                self.bombs = int(self.bombs * random.randint(60, 90) / 100)
+                player.bombs = int(player.bombs * random.randint(30, 60) / 100)
+
+                self.soldiers = int(self.soldiers * random.randint(60, 90) / 100)
+                player.soldiers = int(player.soldiers * random.randint(30, 60) / 100)
+
+                self.meds = int(self.meds * random.randint(60, 90) / 100)
+                player.meds = int(player.meds * random.randint(30, 60) / 100)
+
+                self.fighterJets = int(self.fighterJets * random.randint(60, 90) / 100)
+                player.fighterJets = int(player.fighterJets * random.randint(30, 60) / 100)
+
+                alpha = random.randint(30,60)
+                self.coins += int(alpha*player.coins/100)
+                player.coins = int((100 - alpha)*player.coins/100)
+
+            else:
+                player.victories += 1
+                player.defeats += 1
+
+                player.xp += random.randint(5, 10)
+                self.xp += random.randint(1, 5)
+
+                player.tanks = int(player.tanks * random.randint(60, 90) / 100)
+                self.tanks = int(self.tanks * random.randint(30, 60) / 100)
+
+                player.bombs = int(player.bombs * random.randint(60, 90) / 100)
+                self.bombs = int(self.bombs * random.randint(30, 60) / 100)
+
+                player.soldiers = int(player.soldiers * random.randint(60, 90) / 100)
+                self.soldiers = int(self.soldiers * random.randint(30, 60) / 100)
+
+                player.meds = int(player.meds * random.randint(60, 90) / 100)
+                self.meds = int(self.meds * random.randint(30, 60) / 100)
+
+                player.fighterJets = int(player.fighterJets * random.randint(60, 90) / 100)
+                self.fighterJets = int(self.fighterJets * random.randint(30, 60) / 100)
+
+                alpha = random.randint(30, 60)
+                player.coins += int(alpha * self.coins / 100)
+                self.coins = int((100 - alpha) * self.coins/100)
+
+        else:
+            raise TypeError
 
 
 @client.event
