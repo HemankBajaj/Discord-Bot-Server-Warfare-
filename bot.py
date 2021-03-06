0ff9355db4e65b62fed845610617cc5b413e82b8
Hemank Bajaj <hemankbajaj05@gmail.com>
2021-03-06 20:21:03 +0530

    added env

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
 import discord
 import random
+import os
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     general_channel = client.get_channel(817437302367191123)
-    greeting_msg = ['Yo!' , 'Hello!' , 'Wassup!']
+    greeting_msg = ['Yo!', 'Hello!', 'Wassup!']
     await general_channel.send(random.choice(greeting_msg))
 
 
-client.run('ODE3NjYxOTMzNzM4MTMxNDg2.YEMxBA.sp0aj1JE5Ywmdit4qVj2bTRa9LM')
+client.run(os.getenv('DISCORD_TOKEN'))
+
