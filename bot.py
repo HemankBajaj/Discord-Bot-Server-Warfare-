bfda632a351660a14641011931e362bafd99306c
Hemank Bajaj <hemankbajaj05@gmail.com>
2021-03-06 21:23:49 +0530

    player class creted

--- bot.py
+++ bot.py
@@ -6,14 +6,29 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-
 client = discord.Client()
+
+class Player:
+    def __init__(self, player_name, player_id):
+        self.name = player_name
+        self.id = player_id
+        self.xp = 50                            # for account creation lol
+        self.coins = 2000
+        self.tanks = 20
+        self.fighterJets = 10
+        self.soldiers = 5000
+        self.bombs = 100
+        self.meds =
+        self.victories = 0
+        self.defeats = 0
+        
 
 @client.event
 async def on_ready():
     general_channel = client.get_channel(817437302367191123)
     greeting_msg = ['Yo!', 'Hello!', 'Wassup!']
     await general_channel.send(random.choice(greeting_msg))
+
 
 
 client.run(os.getenv('DISCORD_TOKEN'))
